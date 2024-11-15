62ed74a91127f145f33e0b7f5e3c7da3caa21f5f
JDhyeok <jdhyeok97@gmail.com>
2024-11-15 14:02:05 +0900

    fix: fix name

--- src/main/java/com/example/demo/controller/ReportAnalysisController.java
+++ src/main/java/com/example/demo/controller/ReportAnalysisController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/report")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class ReportAnalysisController {
 
