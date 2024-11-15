21399f53b5b549aa733001f4d3a969928695781e
JDhyeok <jdhyeok97@gmail.com>
2024-11-15 14:07:05 +0900

    fix: fix api uri

--- src/main/java/com/example/demo/controller/ReportAnalysisController.java
+++ src/main/java/com/example/demo/controller/ReportAnalysisController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/report")
 @RequiredArgsConstructor
 public class ReportAnalysisController {
 
